90bb62f545ff95c57ba89d92a665016096e755de
staskachaev <staskachaev205@mail.ru>
2024-10-12 20:56:33 +0700

    v-for skeleton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,27 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-import ProductCard from "@/components/cards/ProductCard.vue";
-import {useCardStore} from "@/stores/CardStore.js";
-import {scrollToTop} from "@/shared/functions.js";
-import ProductCardSkeleton from "@/components/cards/ProductCardSkeleton.vue";
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import ProductCard from '@/components/cards/ProductCard.vue'
+import { useCardStore } from '@/stores/CardStore.js'
+import { scrollToTop } from '@/shared/functions.js'
+import ProductCardSkeleton from '@/components/cards/ProductCardSkeleton.vue'
 
-
-const showScroll = ref(false);
+const showScroll = ref(false)
 
 const cardStore = useCardStore()
-const { getCards } = useCardStore();
+const { getCards } = useCardStore()
 
 const handleScroll = () => {
-  showScroll.value = window.scrollY > window.innerHeight * 0.3;
-};
+  showScroll.value = window.scrollY > window.innerHeight * 0.3
+}
 
-onMounted( async () => {
+onMounted(async () => {
   await getCards()
-  window.addEventListener('scroll', handleScroll);
-});
+  window.addEventListener('scroll', handleScroll)
+})
 
 onBeforeUnmount(() => {
-  window.removeEventListener('scroll', handleScroll);
-});
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
@@ -38,17 +37,18 @@ onBeforeUnmount(() => {
       </template>
       <template v-else>
         <product-card-skeleton
-          v-for="item in 8"
+          v-for="card in cardStore.cards"
+          :key="card"
         />
       </template>
-      
+
       <img
         v-if="showScroll"
         class="scroll-up"
         src="@/assets/images/arrow.svg"
         alt="arrow-up"
         @click="scrollToTop"
-      >
+      />
     </section>
   </div>
 </template>
@@ -56,10 +56,13 @@ onBeforeUnmount(() => {
 <style scoped lang="scss">
 .catalog {
   font-family: $second-font;
-  background: radial-gradient(rgb(147 197 253 / var(--tw-bg-opacity)), $background-color);
+  background: radial-gradient(
+    rgb(147 197 253 / var(--tw-bg-opacity)),
+    $background-color
+  );
   @apply justify-center p-2;
 
-  &-section{
+  &-section {
     @apply items-center;
 
     @media screen and (max-width: 1300px) {
